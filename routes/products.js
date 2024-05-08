be308f03352e68724b151a515246d4c0f3601524
Varinthorn Jaitrong <varinthorn.jaitrong@lotuss.com>
2024-05-08 16:46:33 +0700

    update entpoint to async function

--- routes/products.js
+++ routes/products.js
@@ -3,20 +3,22 @@ const router = express.Router();
 const Product = require('../models/Product');
 const mongoose = require('mongoose');
 
-router.get('/', (req, res, next) => {
-    Product.find().then(products => {
+router.get('/', async (req, res, next) => {
+    try {
+        const products = await Product.find();
         res.json(products);
-    }).catch(err => {
+    } catch (err) {
         next(err);
-    });
+    }
 });
 
-router.get('/:id', (req, res, next) => {
-    Product.findById(req.params.id).then(products => {
-        res.json(products);
-    }).catch(err => {
+router.get('/:id', async (req, res, next) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        res.json(product);
+    } catch (err) {
         next(err);
-    });
+    }
 });
 
 router.post('/', async (req, res, next) => {
