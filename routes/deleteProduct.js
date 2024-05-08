55668cbcac49086e171d80fac57e42a01878d92e
Varinthorn Jaitrong <varinthorn.jaitrong@lotuss.com>
2024-05-08 23:52:26 +0700

    add header field

--- routes/deleteProduct.js
+++ routes/deleteProduct.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const Product = require('../models/Product');
+const setRequestId = require('../middlewares/setRequestId');
+const handleApiKeyValidation = require('../middlewares/handleApiKeyValidation');
+const { handleValidationErrors } = require('../middlewares/validateProductData');
 
-router.delete('/:id', async (req, res, next) => {
+router.delete('/:id' ,setRequestId, handleApiKeyValidation, handleValidationErrors, async (req, res, next) => {
     try {
         const product = await Product.findByIdAndDelete(req.params.id);
         res.status(200).json(product);
